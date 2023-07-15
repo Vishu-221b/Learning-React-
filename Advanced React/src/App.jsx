10dbb9c3db0dc24a929b640b83c3f64c19ae7157
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-07-15 10:03:12 +0530

    Button challenge

---
+++ Advanced React/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css'
+import Button from "./button.jsx";
+
+export default function App(){
+  return (
+
+  <main>
+    <h1> hello</h1>
+    <Button
+    text = "fuck off!!!!!!!!!!!!!!!"
+    />
+</main>
+
+)
+}
